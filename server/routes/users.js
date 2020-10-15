d3370b27339cd0690a23a0933d9c790feefded5b
Bhushan-Koyande <53961964+Bhushan-Koyande@users.noreply.github.com>
2020-10-15 17:15:55 +0530

    initial commit

---
+++ server/routes/users.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const router = express.Router();
+var nodemailer = require('nodemailer');
+const { User } = require("../models/User");
+
+const { auth } = require("../middleware/auth");
+
+//=================================
+//             User
+//=================================
+
+router.get("/auth", auth, (req, res) => {
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image,
+    });
+});
+
+router.post("/register", (req, res) => {
+
+    const user = new User(req.body);
+
+    user.save((err, doc) => {
+        if (err) return res.json({ success: false, err });
+        
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'bhushankoyande446@gmail.com',
+                pass: 'football12345678'
+            }
+        });
+
+        var mailOptions = {
+            from: 'bhushankoyande446@gmail.com',
+            to: doc.email,
+            subject: 'Welcome !',
+            text: 'Thanks for joining us at ChatCord. '
+        };
+
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+
+        return res.status(200).json({
+            success: true,
+            message: "Please check your email"
+        });
+    });
+});
+
+router.post("/login", (req, res) => {
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user)
+            return res.json({
+                loginSuccess: false,
+                message: "Auth failed, email not found"
+            });
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch)
+                return res.json({ loginSuccess: false, message: "Wrong password" });
+
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+                res.cookie("w_authExp", user.tokenExp);
+                res
+                    .cookie("w_auth", user.token)
+                    .status(200)
+                    .json({
+                        loginSuccess: true
+                    });
+            });
+        });
+    });
+});
+
+router.get("/logout", auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id }, { token: "", tokenExp: "" }, (err, doc) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).send({
+            success: true
+        });
+    });
+});
+
+module.exports = router;
